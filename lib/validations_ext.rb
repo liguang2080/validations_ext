c1bae774ed41fd55c1994bccadc1a52d6ea65f56
liguang <lg2046@gmail.com>
2012-01-31 18:20:22 +0800

    change to submodule

--- lib/validations_ext.rb
+++
@@ -1,6 +0,0 @@
-require "validations_ext/version"
-require "validations_ext/file_size_validator"
-
-module ValidationsExt
-  # Your code goes here...
-end
--- lib/validations_ext/version.rb
+++
@@ -1,3 +0,0 @@
-module ValidationsExt
-  VERSION = "0.0.1"
-end
